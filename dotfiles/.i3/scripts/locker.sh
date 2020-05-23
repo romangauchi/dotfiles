a11612a3be91b1f60d2feed28cc793563e41fb5a
romangauchi <roman.gauchi@gmail.com>
2020-05-24 00:50:36 +0200

    locker.sh: update the lock screen with minimalist locker

--- dotfiles/.i3/scripts/locker.sh
+++ dotfiles/.i3/scripts/locker.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
 
-i3lock -i /usr/share/wallpapers/Andromeda/contents/images/1920x1200.png -k -e
+wallpaper="/usr/share/wallpapers/Andromeda/contents/images/1920x1200.png"
+
+i3lock -i $wallpaper \
+    --clock --ignore-empty-password \
+    --radius=120 --ring-width=5 --indicator \
+    --layoutcolor=111111ff --datecolor=111111ff --timecolor=111111ff \
+    --insidecolor=00000000 --linecolor=00000000 --separatorcolor=00000000 \
+    --keyhlcolor=222222aa --bshlcolor=ffb52aaa \
+    --ringcolor=ffffff55 \
+    --insidewrongcolor=00000000 --wrongcolor=00000000 --ringwrongcolor=ff3936aa \
+    --insidevercolor=00000000 --verifcolor=00000000 --ringvercolor=ffb52aaa \
+    --wrongtext="" \
+    --noinputtext="" \
+    --veriftext="Hum..." \
+    --locktext="" \
+    --lockfailedtext="" \
+
+    # --greetertext="Type password to unlock..." \
+    # --greetercolor=222222ff \
+
